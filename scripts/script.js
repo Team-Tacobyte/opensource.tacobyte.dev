d3c6d6cebd6b8b312289aa98d89d983fb0e41cb3
BossFreind1234 <vincentredtube@gmail.com>
2022-03-31 20:06:12 -0400

    just getting started!

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+function jobRedirect() {
+    window.location.href = "https://jobs.tacobyte.dev"
+}
+
+function discordRedirect() {
+    window.location.href = "https://discord.gg/acPGzacTF5 "
+}
+
+function githubRedirect() {
+    window.location.href = "https://github.com/Team-Tacobyte"
+}
+
+function betaSignup() {
+    window.location.href = "/pages/signup.html"
+}
+
+function betaJoin() {
+    window.location.href = "/pages/join.html"
+}
+
+function contribueNow() {
+    window.location.href = "/pages/contribute.html"
+}
